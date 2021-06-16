648ee5a4a74f19434f1246559eed1e6e5b16377b
R K <blah@eample.com>
2021-06-16 11:57:53 +0200

    changed colors, added detail page

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const searchForm = document.getElementById("search-form");
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
 
+// URL der API
+const API_URL =
+  // "https://cors-anywhere.herokuapp.com/...";
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+document.getElementById("results").addEventListener("click", (e) => {
+  e.preventDefault();
+  // console.log(e.target);
+  // console.log(e.target.dataset);
+  const currname = e.target.dataset.currname;
+  console.log(currname);
+  showResult(API_URL, null, null, currname);
+});
+
 searchForm.addEventListener("submit", (e) => {
-  // URL der API
-
-  const URL =
-    // "https://cors-anywhere.herokuapp.com/https://api.coinmarketcap.com/v1/";
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-
   // Get limit
   const searchLimit = document.getElementById("limit").value;
   // Get search
@@ -17,9 +25,9 @@ searchForm.addEventListener("submit", (e) => {
 
   if (searchTerm == "") {
     // Show message
-    showResult(URL, searchLimit);
+    showResult(API_URL, searchLimit);
   } else {
-    showResult(URL, 100, searchTerm.toLowerCase());
+    showResult(API_URL, 100, searchTerm.toLowerCase());
   }
   // Clear field
   searchInput.value = "";
@@ -27,10 +35,13 @@ searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-function showResult(url, limit = 100, search) {
+function showResult(url, limit = 100, search, cname) {
   fetch(url)
     .then((resp) => resp.json())
-    .then((data) => show(data, limit, search));
+    .then((data) => {
+      if (cname) showDetail(data, cname);
+      else show(data, limit, search);
+    });
 }
 
 function show(data, limit, search) {
@@ -43,23 +54,27 @@ function show(data, limit, search) {
 
   data.forEach((post) => {
     output += `
-    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
       <div class="card mb-2">
      
-      <div class="card-body">
-        <h5 class="card-title">${post.name}</h5>
-        <span class="badge badge-info">Rank: ${post.market_cap_rank}</span>
+      <div  class="card-body">
+        <h5 class="card-title"><a data-tooltip="Show Details" data-currname="${
+          post.id
+        }">${post.name}</a></h5>
+        <h4><span class="badge badge-info">Rank: ${
+          post.market_cap_rank
+        }</span></h4>
        
         <hr>
-        <span class="badge badge-secondary">Price USD: ${post.current_price.toFixed(
+        <h4><span class="badge badge-sec">Price USD: ${post.current_price.toFixed(
           2
-        )}</span> 
-        <span class="badge badge-success">24 change: ${post.price_change_24h.toFixed(
+        )}</span></h4> 
+        <h4><span class="badge badge-info">24 change: ${post.price_change_24h.toFixed(
           2
-        )}</span>
-        <span class="badge badge-warning">24h % change: ${post.price_change_percentage_24h.toFixed(
+        )}</span></h4>
+        <h4><span class="badge badge-sec">24h % change: ${post.price_change_percentage_24h.toFixed(
           2
-        )}</span>      
+        )}</span></h4>     
         
       </div>
     </div>
@@ -67,5 +82,72 @@ function show(data, limit, search) {
       `;
   });
   output += "</div>";
+  document.getElementById("results").innerHTML = output;
+}
+
+function showDetail(data, cname) {
+  console.log(data, cname);
+  const post = data.find((el) => el.id == cname);
+  let output = "";
+
+  /* ath: 64805
+ath_change_percentage: -38.576
+ath_date: "2021-04-14T11:54:46.763Z"
+atl: 67.81
+atl_change_percentage: 58602.61315
+atl_date: "2013-07-06T00:00:00.000Z"
+circulating_supply: 18735531
+current_price: 39885
+fully_diluted_valuation: 835918754027
+high_24h: 41117
+id: "bitcoin"
+image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+last_updated: "2021-06-15T12:01:36.944Z"
+low_24h: 39182
+market_cap: 745780082359
+market_cap_change_24h: 9140915980
+market_cap_change_percentage_24h: 1.24089
+market_cap_rank: 1
+max_supply: 21000000
+name: "Bitcoin"
+price_change_24h: 598.65
+price_change_percentage_24h: 1.52382
+roi: null
+symbol: "btc"
+total_supply: 21000000
+total_volume: 43164370255 */
+
+  output += `
+   
+      <div class="card mb-2">
+     
+      <div  class="card-body">
+        <h3 class="card-title"><a data-currname="${post.id}">${
+    post.name
+  }</a></h3>
+        <img src="${post.image}">
+        <h2><span class="badge badge-info">Rank: ${
+          post.market_cap_rank
+        }</span></h2>
+       
+        <hr>
+        <h2><span class="badge badge-sec">Price USD: ${post.current_price.toFixed(
+          2
+        )}</span></h2>
+        <h2><span class="badge badge-info">24 change: ${post.price_change_24h.toFixed(
+          2
+        )}</span></h2>
+        <h2><span class="badge badge-sec">24h % change: ${post.price_change_percentage_24h.toFixed(
+          2
+        )}</span></h2>
+        <h2><span class="badge badge-info">low/high 24h: ${post.low_24h.toFixed(
+          2
+        )} - ${post.high_24h.toFixed(2)}</span></h2>    
+        
+      </div>
+    </div>
+   
+      `;
+
   document.getElementById("results").innerHTML = output;
 }
